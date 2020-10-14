4c7c1acad86646fc3a355b0bad3c4e6fe81fbabc
Patrick Santos <patrickspteixeira@gmail.com>
2020-10-14 12:21:42 -0300

    altera url base da documentação

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Inovando', // Title for your website.
   tagline: 'Documentação de boas práticas e templates',
-  url: 'https://inovando.github.io/', // Your website URL
+  url: 'https://inovando.github.io/docs/', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
