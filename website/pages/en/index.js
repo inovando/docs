a5e769ac0b1b6eb791bdb1aaa90204596e86769d
Patrick Santos <patrickspteixeira@gmail.com>
2020-10-13 21:35:01 -0300

    ajusta textos e imagens de introdução

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -88,26 +88,10 @@ class Index extends React.Component {
         {[
           {
             content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
-            image: `${baseUrl}img/undraw_code_review.svg`,
+              'Collection e environment do Postman testados com garantia da qualidade dos endpoints.',
+            image: `${baseUrl}img/postman.gif`,
             imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations',
-          },
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/undraw_note_list.svg`,
-            imageAlign: 'right',
-            title: 'Description',
+            title: 'Postman 100% testado',
           },
         ]}
       </Block>
@@ -118,10 +102,10 @@ class Index extends React.Component {
         {[
           {
             content:
-              'Each new Docusaurus project has **randomly-generated** theme colors.',
-            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
+              'Reconhece algum desses logotipos? Então você já está (quase) pronto pra aproveitar melhor cada template que desenvolvemos no formato open source! Isso mesmo! Você também pode contribuir com os códigos presentes nessa documentação e nos repositórios da nossa organização do github.',
+            image: `${baseUrl}img/techs.svg`,
             imageAlign: 'right',
-            title: 'Randomly Generated Theme Colors',
+            title: 'Tecnologias adotadas pela empresa',
           },
         ]}
       </Block>
@@ -152,36 +136,6 @@ class Index extends React.Component {
       </Block>
     );
 
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter((user) => user.pinned)
-        .map((user) => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = (page) =>
-        baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    };
-
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
@@ -189,8 +143,6 @@ class Index extends React.Component {
           <Features />
           <LearnHow />
           <TryOut />
-          <Description />
-          <Showcase />
         </div>
       </div>
     );
